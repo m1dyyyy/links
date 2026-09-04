5f0328b377c18b5e862efb42d098992027ae8082
m1dyyyy <142227657+m1dyyyy@users.noreply.github.com>
2026-09-04 10:46:40 +0300

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,12 @@
 export const metadata = {
-  title: 'VoteArt',
-  description: 'VoteArt Link Protection System',
+  title: 'Загрузка...',
+  description: 'Пожалуйста, подождите',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ru">
-      <body style={{ margin: 0, padding: 0, backgroundColor: '#080c14' }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
