4e1101b8514060d60a906bac24e9cdf851c94936
m1dyyyy <142227657+m1dyyyy@users.noreply.github.com>
2026-09-07 14:12:35 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -55,7 +55,7 @@ export default function Home() {
   };
 
   const handleCopy = (subdomain, index) => {
-    const fullUrl = `https://${subdomain}.mirbe.sbs`;
+    const fullUrl = `https://${subdomain}.quartiza.sbs`;
     navigator.clipboard.writeText(fullUrl);
     setCopiedIndex(index);
     setTimeout(() => setCopiedIndex(null), 2000);
@@ -211,7 +211,7 @@ export default function Home() {
             </div>
           ) : (
             links.map((item, idx) => {
-              const fullLink = `https://${item.subdomain}.mirbe.sbs`;
+              const fullLink = `https://${item.subdomain}.quartiza.sbs`;
               const clicksCount = item.clicks || 0;
 
               return (
